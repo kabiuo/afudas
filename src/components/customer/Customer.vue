a69ddbdf1302841a6ca06a3f65c7c6deacdaafe7
kabiuo <852119338@qq.com>
2023-04-27 17:35:26 +0800

    add customer page Optional query parameters;

--- src/components/customer/Customer.vue
+++ src/components/customer/Customer.vue
@@ -1,49 +1,34 @@
 <template xmlns="http://www.w3.org/1999/html">
     <a-card :bordered="true">
-        <el-form>
+        <el-form :model="formCustomer">
             <el-row :gutter="20">
                 <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
-                    <el-form-item label="Activity name">
-                        <el-input/>
+                    <el-form-item label="CustomerName：">
+                        <el-input v-model="formCustomer.sysCustomerName"/>
+                    </el-form-item>
+                </el-col>
+                <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
+                    <el-form-item label="IdcardNumber">
+                        <el-input v-model="formCustomer.sysCustomerIdcardNumber"/>
                     </el-form-item>
                 </el-col>
                 <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
                     <el-form-item label="Activity name">
-                        <el-input/>
-                    </el-form-item>
-                </el-col>
-                <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
-                    <el-form-item label="Activity name">
-                        <el-input/>
+                        <el-input v-model="formCustomer.sysCustomerPhone"/>
                     </el-form-item>
                 </el-col>
             </el-row>
+
             <el-row :gutter="20">
                 <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
-                    <el-form-item label="Activity name">
-                        <el-input/>
-                    </el-form-item>
+                    <!--                    占位-->
                 </el-col>
                 <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
-                    <el-form-item label="Activity name">
-                        <el-input/>
-                    </el-form-item>
-                </el-col>
-                <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
-                    <el-form-item label="Activity name">
-                        <el-input/>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-            <el-row :gutter="20">
-                <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
-                    <el-form-item label="Activity name">
-                        <el-input/>
-                    </el-form-item>
+                    <!--                    占位-->
                 </el-col>
                 <el-col :xs="4" :sm="6" :md="8" :lg="8" :xl="11">
                     <el-form-item>
-                        <el-button type="primary" @click="">Query</el-button>
+                        <el-button type="primary" @click="queryEligibleUserInfo('formCustomer')">Query</el-button>
                     </el-form-item>
                 </el-col>
             </el-row>
@@ -59,8 +44,9 @@
             <el-table-column prop="sysCustomerCreateTime" label="CreateTime" width="120"/>
             <el-table-column prop="sysCustomerUpdateTime" label="UpdateTime" width="120"/>
             <el-table-column fixed="right" label="Operations" width="120">
-                <template #default>
-                    <el-button link type="primary" size="small" @click="">Detail</el-button>
+                <template #default="scope">
+                    <el-button link type="primary" size="small" @click.prevent="handleClickDelete(scope.row)">Detail
+                    </el-button>
                     <el-button link type="primary" size="small">Edit</el-button>
                 </template>
             </el-table-column>
@@ -93,6 +79,12 @@ export default defineComponent({
     components: {},
     data() {
         return {
+            formCustomer: {
+                sysCustomerIdcardNumber: "",
+                sysCustomerName: "",
+                sysCustomerPhone: "",
+                sysCustomerSex: "",
+            },
             tableData: [],
             currentPage: 1,
             pageSize: 10,
@@ -100,23 +92,19 @@ export default defineComponent({
         };
     },
     created() {
-        this.getAllCustomUser();
+        this.tableData = null;
     },
     methods: {
-        getAllCustomUser() {
+        queryEligibleUserInfo() {
             let _this = this;
-            axios({
-                method: 'get',
-                url: '/customer/customer-user/getallcustomer/' + _this.currentPage + '/' + _this.pageSize,
-            })
+            axios.post('/customer/customer-user/geteligibleuser/' + _this.currentPage + '/' + _this.pageSize, _this.formCustomer)
                 .then(function (response) {
-                    if (_this.tableData === null && response.data.length >= 2) {
+                    if (response.data.length >= 2) {
                         _this.tableData = null;
                         _this.tableData = response.data[0];
                         _this.pageTotal = response.data[1];
                     } else {
                         _this.tableData = null;
-                        _this.getAllCustomUser();
                     }
                 });
         },
@@ -131,6 +119,35 @@ export default defineComponent({
             this.currentPage = val;
             this.tableData = null;
             this.getAllCustomUser();
+        },
+        handleClickDelete(row) {
+            console.log(row)
+            let _this = this;
+            this.$confirm('此操作将永久删除该用户信息, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            })
+            // .then(() => {
+            //     axios.delete('/admin/admin-user/deleteadminuser/' + row.sysCustomerId).then(function (res) {
+            //         if (res.data) {
+            //             _this.$message({
+            //                 type: 'success',
+            //                 message: '删除成功!'
+            //             });
+            //             _this.getAllAdminInfo();
+            //         } else {
+            //             _this.$message.error('删除失败！');
+            //         }
+            //     }).catch(function (error) {
+            //         _this.$message.error('网络错误，请检查网络连接！');
+            //     });
+            // }).catch(() => {
+            //     _this.$message({
+            //         type: 'info',
+            //         message: '已取消删除'
+            //     });
+            // });
         }
 
     },
